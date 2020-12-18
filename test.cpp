3ad3fc6f1c29cd3bd63c12ecbf3842ebd05ecb9b
e <ffpsping@163.com>
2020-12-18 18:10:25 +0800

    delete test.c

--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
-using namespace std;
+using namespace::std;
 int main(){
-	cout<<"Hello World！！！";
+    
+    cout<<"Hello";
+    system("pause");
 }
---
+++ test_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    printf("TEST!!!");
+    system("pause");
+    return 0;
+    
+}
+
